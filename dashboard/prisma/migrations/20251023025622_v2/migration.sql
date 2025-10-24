a74d74620b4afd74040a1b60e2443b7a700da67d
Knartzpirat <lasse.schnitker@gmx.de>
2025-10-24 21:25:49 +0200

    Remove obsolete migration files and drop unused columns from setup_status table

--- dashboard/prisma/migrations/20251023025622_v2/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `schemaVersion` on the `setup_status` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "setup_status" DROP COLUMN "schemaVersion";
