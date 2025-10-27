b00eb173a15ef20c86bc5c89416be3b842a4b49e
Knartzpirat <lasse.schnitker@gmx.de>
2025-10-27 04:47:27 +0100

    Refactor import statements and improve formatting in TestObjects page

--- dashboard/app/dashboard/test-objects/page.tsx
+++ dashboard/app/dashboard/test-objects/page.tsx
@@ -1,33 +1,42 @@
-
-import { Suspense } from "react";
-import { DataTableSkeleton } from "@/components/data-table/data-table-skeleton";
-import { FeatureFlagsProvider } from "@/components/data-table/feature-flags-provider";
-import { }
+import { Suspense } from 'react';
+import { DataTableSkeleton } from '@/components/data-table/data-table-skeleton';
+import { FeatureFlagsProvider } from '@/components/data-table/feature-flags-provider';
+import { searchParamsCache } from '@/lib/validations/params';
+import { getValidFilters } from '@/lib/parsers';
+import { getTestObjects } from './_lib/queries';
+import { TestObjectsTable } from './_components/test-objects-table';
 import type { SearchParams } from '@/types';
+
 interface TestObjectsProps {
   searchParams: Promise<SearchParams>;
 }
 
 export default function IndexPage(props: TestObjectsProps) {
   return (
-
     <Suspense
-        fallback={
-            <DataTableSkeleton
-            columnCount={7}
-            filterCount={2}
-            cellWidths={['10rem', '30rem', '10rem', '10rem', '6rem', '6rem', '6rem']}
-            shrinkZero
-            />
-        }
+      fallback={
+        <DataTableSkeleton
+          columnCount={7}
+          filterCount={2}
+          cellWidths={[
+            '10rem',
+            '30rem',
+            '10rem',
+            '10rem',
+            '6rem',
+            '6rem',
+            '6rem',
+          ]}
+          shrinkZero
+        />
+      }
     >
-        <FeatureFlagsProvider tableId="testObjects">
-            <TestObjectsTableWrapper {...props} />
-        </FeatureFlagsProvider>
+      <FeatureFlagsProvider tableId="testObjects">
+        <TestObjectsTableWrapper {...props} />
+      </FeatureFlagsProvider>
     </Suspense>
-    );
+  );
 }
-
 
 async function TestObjectsTableWrapper(props: TestObjectsProps) {
   const searchParams = await props.searchParams;
@@ -36,13 +45,10 @@ async function TestObjectsTableWrapper(props: TestObjectsProps) {
   const validFilters = getValidFilters(search.filters);
 
   const promises = Promise.all([
-    getTasks({
+    getTestObjects({
       ...search,
       filters: validFilters,
     }),
-    //getTaskStatusCounts(),
-    //getTaskPriorityCounts(),
-    //getEstimatedHoursRange(),
   ]);
 
   return <TestObjectsTable promises={promises} />;
