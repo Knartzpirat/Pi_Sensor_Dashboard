c24f4ad84b1ecb14167136340003fc2f1f0eb5da
Knartzpirat <lasse.schnitker@gmx.de>
2025-10-27 04:47:32 +0100

    Add TestObjectsTable component for displaying test object data

---
+++ dashboard/app/dashboard/test-objects/_components/test-objects-table.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import * as React from 'react';
+import { DataTable } from '@/components/data-table/data-table';
+import { DataTableToolbar } from '@/components/data-table/data-table-toolbar';
+import { useDataTable } from '@/hooks/use-data-table';
+import type { TestObjectsTableData } from '@/types/test-object';
+import { getColumns } from '../_lib/columns';
+
+interface TestObjectsTableProps {
+  promises: Promise<
+    [
+      {
+        data: TestObjectsTableData[];
+        total: number;
+      }
+    ]
+  >;
+}
+
+export function TestObjectsTable({ promises }: TestObjectsTableProps) {
+  const [{ data, total }] = React.use(promises);
+
+  const columns = React.useMemo(() => getColumns(), []);
+
+  const pageCount = Math.ceil(total / 10);
+
+  const { table } = useDataTable({
+    data,
+    columns,
+    pageCount,
+    initialState: {
+      pagination: {
+        pageIndex: 0,
+        pageSize: 10,
+      },
+    },
+  });
+
+  return (
+    <div className="space-y-4">
+      <DataTable table={table}>
+        <DataTableToolbar table={table} />
+      </DataTable>
+    </div>
+  );
+}
